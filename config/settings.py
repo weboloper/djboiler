ffa8cf35df98a66a2c7402858969b4990974d5d2
weboloper <weboloper@gmail.com>
2025-03-24 15:44:05 +0300

    updates

--- config/settings.py
+++ config/settings.py
@@ -405,9 +405,31 @@ CKEDITOR_5_ALLOW_ALL_FILE_TYPES = True
 CKEDITOR_5_UPLOAD_FILE_TYPES = ['jpeg', 'pdf', 'png'] # optional
 CKEDITOR_5_CONFIGS = {
   'default': {
-      'toolbar': ['heading', '|', 'bold', 'italic', 'link',
-                  'bulletedList', 'numberedList', 'blockQuote', 'imageUpload', 'fileUpload' ], # include fileUpload here
+      'toolbar': ['heading', '|', 'essentials', 'code', 'imageStle', 'image', 'bold', 'italic', 'link',
+                  'bulletedList', 'numberedList', 'blockQuote', 'imageUpload', 'fileUpload', 'alignment', 'imageToolbar', 'mediaEmbed','imageInsert' ], # include fileUpload here
       'language': 'tr',
-}
+    },
+    'extends': {
+        'blockToolbar': [
+            'paragraph', 'heading1', 'heading2', 'heading3',
+            '|',
+            'bulletedList', 'numberedList',
+            '|',
+            'blockQuote',
+        ],
+ 
+        'image': {
+            'toolbar': ['imageTextAlternative', '|', 'imageStyle:alignLeft',
+                        'imageStyle:alignRight', 'imageStyle:alignCenter', 'imageStyle:side',  '|'],
+            'styles': [
+                'full',
+                'side',
+                'alignLeft',
+                'alignRight',
+                'alignCenter',
+            ]
+
+        }
+    }
 }
 CK_EDITOR_5_UPLOAD_FILE_VIEW_NAME = "custom_upload_file"
